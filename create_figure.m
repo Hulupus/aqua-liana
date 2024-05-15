da11632d99f47b756c30a374d9814b070075fb27
Hulupus <117013418+Hulupus@users.noreply.github.com>
2024-05-15 09:07:27 +0200

    Update create_figure.m
    Added comments to plan function

--- create_figure.m
+++ create_figure.m
@@ -1,5 +1,23 @@
 function create_figure()
 
+% calc mean, create linear function
+
+% for {
+
+% range begin at last point
+
+% while {
+% calc time between
+% if time < 60 Min
+% add time to range
+% }
+
+% plot(range)
+% hold;
+}
+
+% plot linear function
+
 % Create figure
 figure;
 
